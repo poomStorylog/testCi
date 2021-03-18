0ec775ca289a8fe4ee73acf46d1c20868bb99e45
Poom <poom@storylog.co>
2021-03-18 13:44:59 +0700

    aaaa

--- app/src/main/java/com/example/testci/MainActivity.kt
+++ app/src/main/java/com/example/testci/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     var a = 2
     var b = 2
+    var c = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
